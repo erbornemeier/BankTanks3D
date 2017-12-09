c09eba699442bcb50d9a7327f1ad896a0360e29c
erbornemeier <erbornemeier@mines.edu>
2017-12-09 00:44:13 -0700

    Added enemy roaming tank class as a copy of the player class until new implementation is needed

---
+++ src/EnemyRoamerTank.cpp
@@ -0,0 +1,80 @@
+#include "EnemyRoamerTank.h"
+
+EnemyRoamerTank::EnemyRoamerTank(CSCI441::ModelLoader* tankBase, CSCI441::ModelLoader* tankTurret, 
+					   glm::vec3 pos, glm::vec3 rot, glm::vec3 scl){
+	this->base = tankBase;
+	this->turret = tankTurret;
+
+	this->position = pos;
+	this->baseRotation = rot;
+	this->scale = scl;
+}
+
+void EnemyRoamerTank::moveForward(float tstep){
+	position.z += SPEED * tstep * cos(baseRotation.y);
+	position.x += SPEED * tstep * sin(baseRotation.y);
+}
+
+void EnemyRoamerTank::moveBackward(float tstep){
+	position.z -= SPEED * tstep * cos(baseRotation.y);
+	position.x -= SPEED * tstep * sin(baseRotation.y);
+}
+
+void EnemyRoamerTank::rotateRight(float tstep){
+	baseRotation.y -= ROT_SPEED * tstep;
+	turretRotation.y -= ROT_SPEED * tstep;
+}
+
+void EnemyRoamerTank::rotateLeft(float tstep){
+	baseRotation.y += ROT_SPEED * tstep;
+	turretRotation.y += ROT_SPEED * tstep;
+
+}
+
+void EnemyRoamerTank::rotateTurretRight(float tstep){
+	turretRotation.y -= ROT_SPEED * tstep;
+}
+
+void EnemyRoamerTank::rotateTurretLeft(float tstep){
+	turretRotation.y += ROT_SPEED * tstep;
+}
+
+void EnemyRoamerTank::setScale(glm::vec3 scale){
+	this->scale = scale;
+}
+
+void EnemyRoamerTank::drawBase(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
+		    GLint md_loc, GLint ms_loc, GLint s_loc, GLint ma_loc, GLint txtr){
+
+	base->draw( vpos_loc, vnorm_loc, vtex_loc, md_loc, ms_loc, s_loc, ma_loc, txtr);
+}
+
+void EnemyRoamerTank::drawTurret(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
+		    GLint md_loc, GLint ms_loc, GLint s_loc, GLint ma_loc, GLint txtr){
+
+	turret->draw( vpos_loc, vnorm_loc, vtex_loc, md_loc, ms_loc, s_loc, ma_loc, txtr);
+}
+
+glm::mat4 EnemyRoamerTank::getModelMatrix(){
+	glm::mat4 m;
+	m = glm::scale(m, scale);
+	m = glm::translate(m, position);
+	m = glm::rotate(m, baseRotation.y, glm::vec3(0,1,0));
+	return m;
+}
+
+glm::mat4 EnemyRoamerTank::getTurretModelMatrix(){
+	glm::mat4 m;
+	m = glm::scale(m, scale);
+	m = glm::translate(m, position + glm::vec3(0,0.1,0));
+	m = glm::rotate(m, turretRotation.y, glm::vec3(0,1,0));
+	return m;
+}
+
+glm::vec3 EnemyRoamerTank::getPosition(){
+	return position;
+}
+
+glm::vec3 EnemyRoamerTank::getBaseRotation(){
+	return baseRotation;
+}
---
+++ src/include/EnemyRoamerTank.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <CSCI441/modelLoader3.hpp>
+
+#include <iostream>
+
+class EnemyRoamerTank{
+
+public:
+
+	EnemyRoamerTank(CSCI441::ModelLoader* tankBase,
+			   CSCI441::ModelLoader* tankTurret, 
+			   glm::vec3 pos = glm::vec3(0,0,0), 
+			   glm::vec3 rot = glm::vec3(0,0,0),
+			   glm::vec3 scl = glm::vec3(1,1,1));
+	
+
+	void moveForward(float tstep);
+	void moveBackward(float tstep);
+	void rotateLeft(float tstep);
+	void rotateRight(float tstep);
+	void rotateTurretLeft(float timeStep);
+	void rotateTurretRight(float timeStep);
+	void setScale(glm::vec3 scale);
+
+	void drawBase(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
+		      GLint md_loc, GLint ms_loc, GLint s_loc, GLint ma_loc, GLint txtr);
+	void drawTurret(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
+		      GLint md_loc, GLint ms_loc, GLint s_loc, GLint ma_loc, GLint txtr);
+
+	glm::vec3 getPosition();
+	glm::vec3 getBaseRotation();
+	
+	glm::mat4 getModelMatrix();
+	glm::mat4 getTurretModelMatrix();
+
+private:
+
+	//physical properties about the model
+	glm::vec3 position, baseRotation, turretRotation, scale; 
+
+	//transform matrix to handle translation, rotation, and scaling associated to the model
+	glm::mat4 modelMatrix;
+
+	//storage location for the drawable model
+	CSCI441::ModelLoader *base   = NULL,
+						 *turret = NULL;
+
+	const float SPEED = 5.0f, ROT_SPEED = 2.5f;
+};  		
