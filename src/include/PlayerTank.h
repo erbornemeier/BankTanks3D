498866484e083b03efe60002d6c976fcf35e44c8
erbornemeier <erbornemeier@mines.edu>
2017-12-13 14:14:56 -0700

    Updated player tank to hold block colliders from the walls with some simple collision detection that lets the player smoothly slide on the walls without going through them. Need to continue this to do a bounding box as right now its a hardcoded 5x5 cube

--- src/include/PlayerTank.h
+++ src/include/PlayerTank.h
@@ -9,6 +9,8 @@
 #include <CSCI441/modelLoader3.hpp>
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 class PlayerTank{
 
@@ -29,6 +31,9 @@ public:
 	void rotateTurretRight(float timeStep);
 	void setScale(glm::vec3 scale);
 
+	bool isColliding();
+	void setBlockColliders(vector<glm::vec3>& blockPos, const float& blockDim);
+
 	void drawBase(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
 		      GLint md_loc, GLint ms_loc, GLint s_loc, GLint ma_loc, GLint txtr);
 	void drawTurret(GLint vpos_loc, GLint vnorm_loc, GLint vtex_loc, 
@@ -45,6 +50,13 @@ private:
 
 	//physical properties about the model
 	glm::vec3 position, baseRotation, turretRotation, scale; 
+
+	//bounding box properties
+	float tankLength, tankWidth;
+
+	//colliders
+	vector<glm::vec3> blockColliders;
+	float blockSize;
 
 	//transform matrix to handle translation, rotation, and scaling associated to the model
 	glm::mat4 modelMatrix;
