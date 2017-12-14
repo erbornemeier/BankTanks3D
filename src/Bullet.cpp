9492161c3b81a56a31ca78486b36a65e8c9a5767
Eric Olson <eric.olson.411@gmail.com>
2017-12-13 23:56:30 -0700

    bouncing

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -1,6 +1,8 @@
 #include "Bullet.h"
 #include "Level.h"
 
+#include <stdlib.h>
+
 Bullet::Bullet(CSCI441::ModelLoader* base, glm::vec3 pos, glm::vec3 dir, int maxBounces, float speed){
 	this->base = base;
 
@@ -29,16 +31,19 @@ void Bullet::setPosition(glm::vec3 pos){
 
 bool Bullet::bounce(vector<glm::vec3>& wallLocations) {
   bool collisionDetected = false;
+  bool leftRight = false;
   for (glm::vec3& wall : wallLocations) {
     if (position.x >= wall.x - Level::BLOCK_DIM / 2 && position.x <= wall.x + Level::BLOCK_DIM / 2
      && position.z >= wall.z - Level::BLOCK_DIM / 2 && position.z <= wall.z + Level::BLOCK_DIM / 2) {
       collisionDetected = true;
+      if (abs(position.x - wall.x) > abs(position.z - wall.z)) {
+        leftRight = true;
+      }
       break;
     }
   }
 
   if (collisionDetected) {
-    return true;
     // update bounce counter
     bounces += 1;
     // if counter is exceeded, return true to signal application to delete bullet
@@ -46,9 +51,20 @@ bool Bullet::bounce(vector<glm::vec3>& wallLocations) {
       return true;
     }
 
-    // to bounce, flip along x or z plane depending on plane bouncing off
+    if (leftRight) {
+      direction.y = M_PI * 2 - direction.y;
+      if (direction.y < 0) {
+        direction.y += 2 * M_PI;
+      }
+    }
+    else {
+      direction.y = M_PI - direction.y;
+      if (direction.y < 0) {
+        direction.y += 2 * M_PI;
+      }
+    }
   }
-
+  return false;
 }
 
 glm::mat4 Bullet::getModelMatrix(){
